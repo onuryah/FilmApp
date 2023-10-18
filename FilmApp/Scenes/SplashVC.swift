af9e6b57d1672eb7eafc8dc125215101d2bb4fc8
OnurAlp <>
2023-10-18 18:13:06 +0300

    Organized project for MVVM Architecture

---
+++ FilmApp/Scenes/SplashVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmApp
+//
+//  Created by OnurAlp on 18.10.2023.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
